775098f4bc5ef543560392b555238be7a995a5c0
Restyled.io <commits@restyled.io>
2024-05-27 15:58:21 +0000

    Restyled by pg_format

--- internal/query.sql
+++ internal/query.sql
@@ -1,19 +1,12 @@
 -- name: InsertUser :exec
-INSERT INTO
-    users (
-        id,
-        auth_id,
-        email,
-        name,
-        role,
-        image_url,
-        created_at
-    )
-VALUES ($1, $2, $3, $4, $5, $6, NOW());
+INSERT INTO users (id, auth_id, email, name, ROLE, image_url, created_at)
+    VALUES ($1, $2, $3, $4, $5, $6, NOW());
 
 -- name: GetAdmin :many
-SELECT *
-FROM users
+SELECT
+    *
+FROM
+    users
 WHERE
     users.email = $1
     AND users.password = $2
@@ -21,63 +14,72 @@ WHERE
 LIMIT 1;
 
 -- name: GetAllUsers :many
-SELECT * from users;
+SELECT
+    *
+FROM
+    users;
 
 -- name: GetUserByAuthID :one
-SELECT * FROM users WHERE users.auth_id = $1 LIMIT 1;
+SELECT
+    *
+FROM
+    users
+WHERE
+    users.auth_id = $1
+LIMIT 1;
+
 -- name: UpdateUser :exec
-UPDATE users
+UPDATE
+    users
 SET
     name = $1,
     image_url = $2,
     updated_at = NOW()
 WHERE
     id = $3;
+
 -- name: DeleteUser :exec
-UPDATE users SET deleted_at = NOW() WHERE id = $1;
+UPDATE
+    users
+SET
+    deleted_at = NOW()
+WHERE
+    id = $1;
 
 -- name: InsertCategory :exec
-INSERT INTO categories (id, name, created_at) VALUES ($1, $2, NOW());
+INSERT INTO categories (id, name, created_at)
+    VALUES ($1, $2, NOW());
+
 -- name: UpdateCategory :exec
-UPDATE categories
+UPDATE
+    categories
 SET
     name = $1,
     updated_at = NOW()
 WHERE
     id = $2;
+
 -- name: DeleteCategory :exec
-UPDATE categories SET deleted_at = NOW() WHERE id = $1;
+UPDATE
+    categories
+SET
+    deleted_at = NOW()
+WHERE
+    id = $1;
 
 -- name: GetAllCategories :many
-SELECT * from categories;
+SELECT
+    *
+FROM
+    categories;
 
 -- name: InsertNews :exec
-INSERT INTO
-    news (
-        id,
-        author,
-        title,
-        description,
-        content,
-        url,
-        image_url,
-        publish_at,
-        created_at
-    )
-VALUES (
-        $1,
-        $2,
-        $3,
-        $4,
-        $5,
-        $6,
-        $7,
-        $8,
-        NOW()
-    );
+INSERT INTO news (id, author, title, description, content, url, image_url, publish_at, created_at)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, NOW());
 
 -- name: UpdateNews :exec
-UPDATE news
+UPDATE
+    news
 SET
     title = $1,
     description = $2,
@@ -89,23 +91,40 @@ SET
     updated_at = NOW()
 WHERE
     id = $8;
+
 -- name: DeleteNews :exec
-UPDATE news SET deleted_at = NOW() WHERE id = $1;
+UPDATE
+    news
+SET
+    deleted_at = NOW()
+WHERE
+    id = $1;
 
 -- name: GetAllNews :many
-SELECT * from news;
+SELECT
+    *
+FROM
+    news;
 
 -- name: InsertLike :exec
-INSERT INTO likes (news_id, user_id) VALUES ($1, $2);
+INSERT INTO likes (news_id, user_id)
+    VALUES ($1, $2);
 
 -- name: DeleteLike :exec
-DELETE from likes Where news_id = $1 and user_id = $2;
+DELETE FROM likes
+WHERE news_id = $1
+    AND user_id = $2;
 
 -- name: InsertDisLike :exec
-INSERT INTO likes (news_id, user_id) VALUES ($1, $2);
+INSERT INTO likes (news_id, user_id)
+    VALUES ($1, $2);
 
 -- name: DeleteDisLike :exec
-DELETE from likes Where news_id = $1 and user_id = $2;
+DELETE FROM likes
+WHERE news_id = $1
+    AND user_id = $2;
 
 -- name: InsertHasCategory :exec
-INSERT INTO has_categories (news_id, category_id) VALUES ($1, $2);
+INSERT INTO has_categories (news_id, category_id)
+    VALUES ($1, $2);
+
